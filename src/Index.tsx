678e64a9f337e205d2bbac3724138bb3182f7c0a
JeanMichaelC <JeanMichaelC@randomemail.com>
2022-09-20 21:38:22 +0200

    :tada: Fine Project Setup 👌
    Perfect Setup for a project with TS, React and custom Webpack

---
+++ src/Index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import { App } from "./App";
+
+const rootNode = document.getElementById('root');
+
+//Null Check for the root node
+if (rootNode) {
+	createRoot(rootNode).render(<App />);
+}

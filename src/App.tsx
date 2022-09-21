5546c9fb89e25e9adeeb9933bf90ece3cb41310e
JeanMichaelC <JeanMichaelC@randomemail.com>
2022-09-21 21:46:39 +0200

    :poop: StyledComponents Test
    Just some crappy code to try StyledComponents with TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import styled from "styled-components";
 
-const App: React.FC = () => {
+const Header1 = styled.h1<{greenMode?: boolean}>`
+	width: 100%;
+	text-align: center;
+	color: ${({greenMode}) => greenMode === true ? 'green' : 'darkgrey'};
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 26px;
+`;
+
+interface AppI {
+	message: string,
+	greenMode?: boolean
+}
+
+const App: React.FC<AppI> = ({ message, greenMode }) => {
+	console.log(greenMode);
 	return(
-		<h1>Hello new React with .babelrc</h1>
+		<Header1 greenMode={greenMode}>Hello new React. Message: {message}</Header1>
 	)
 }
 
